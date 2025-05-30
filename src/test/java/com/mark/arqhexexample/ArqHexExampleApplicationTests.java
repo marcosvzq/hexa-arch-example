e18ba7082a6245a5406654fa5db2bcb87a53da99
Marcos-VG <marcos.sis90@gmail.com>
2025-05-29 21:37:49 -0600

    Initial project setup: Initialize Git and basic project structure

---
+++ src/test/java/com/mark/arqhexexample/ArqHexExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark.arqhexexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqHexExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
